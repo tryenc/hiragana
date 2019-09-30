e60ff535973e59bd85bf033a7ec53d823c1522e2
Tryens, Christopher <tryenc@gmail.com>
2019-09-30 16:20:18 -0400

    Updates

---
+++ precache-manifest.15f70b7e38aa382e1306a8db9913293d.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ab66366345dcd40e4eac5ead8c15a5",
+    "url": "/learn-hiragana/index.html"
+  },
+  {
+    "revision": "6d75b1fee6f57f797193",
+    "url": "/learn-hiragana/static/css/main.daa0b5fc.chunk.css"
+  },
+  {
+    "revision": "d2f5eb85bb4f36293bc0",
+    "url": "/learn-hiragana/static/js/2.013f8dc7.chunk.js"
+  },
+  {
+    "revision": "6d75b1fee6f57f797193",
+    "url": "/learn-hiragana/static/js/main.0950a149.chunk.js"
+  },
+  {
+    "revision": "7d3ef92753b1ac2f376e",
+    "url": "/learn-hiragana/static/js/runtime~main.ec4dfeb5.js"
+  },
+  {
+    "revision": "4b1744dab85c8ad1b4064e955f1dcdca",
+    "url": "/learn-hiragana/static/media/correct.4b1744da.wav"
+  },
+  {
+    "revision": "2f5b4ac0488477b9402041926283469f",
+    "url": "/learn-hiragana/static/media/incorrect.2f5b4ac0.wav"
+  },
+  {
+    "revision": "b91a755683c40cc647403f7f6ba45276",
+    "url": "/learn-hiragana/static/media/play-circle.b91a7556.svg"
+  },
+  {
+    "revision": "cc24e9464d45e9e9f8a7f5566ed9726e",
+    "url": "/learn-hiragana/static/media/あ.cc24e946.ogg"
+  },
+  {
+    "revision": "586ef7cae9ad0ab0e7020275dd4e4a4b",
+    "url": "/learn-hiragana/static/media/い.586ef7ca.ogg"
+  },
+  {
+    "revision": "6daa81f18faac687089be4f53d880eee",
+    "url": "/learn-hiragana/static/media/う.6daa81f1.ogg"
+  },
+  {
+    "revision": "2552b129bc7ec9490bf9604cf72cd454",
+    "url": "/learn-hiragana/static/media/え.2552b129.ogg"
+  },
+  {
+    "revision": "376baa17964df20e92f5cf996ce82fa3",
+    "url": "/learn-hiragana/static/media/お.376baa17.ogg"
+  },
+  {
+    "revision": "19a938fc89f0555cc50a813417ee1fd7",
+    "url": "/learn-hiragana/static/media/か.19a938fc.ogg"
+  },
+  {
+    "revision": "cb7963bf76b1ee3a2ac5a3a96a60fd7e",
+    "url": "/learn-hiragana/static/media/き.cb7963bf.ogg"
+  },
+  {
+    "revision": "ffced3b61c3f12a51e32635fbe9f458f",
+    "url": "/learn-hiragana/static/media/く.ffced3b6.ogg"
+  },
+  {
+    "revision": "4d64edcdefe2eca8926af73236293606",
+    "url": "/learn-hiragana/static/media/け.4d64edcd.ogg"
+  },
+  {
+    "revision": "b9c947cde54b68463c12b0f29ec2158f",
+    "url": "/learn-hiragana/static/media/こ.b9c947cd.ogg"
+  },
+  {
+    "revision": "092649a695d541f824e82e6882391b55",
+    "url": "/learn-hiragana/static/media/さ.092649a6.ogg"
+  },
+  {
+    "revision": "d45e5184a48d78705f126bd559c14a00",
+    "url": "/learn-hiragana/static/media/し.d45e5184.ogg"
+  },
+  {
+    "revision": "f26a525517e5bfb88814cc9fb7792f5d",
+    "url": "/learn-hiragana/static/media/す.f26a5255.ogg"
+  },
+  {
+    "revision": "003032093c2385bdebd56025f55bd28d",
+    "url": "/learn-hiragana/static/media/せ.00303209.ogg"
+  },
+  {
+    "revision": "b8392dd4e0185d57400c1ca75a01035e",
+    "url": "/learn-hiragana/static/media/そ.b8392dd4.ogg"
+  },
+  {
+    "revision": "2d7ad3e9fd082a5d1816afd7c9e23ad1",
+    "url": "/learn-hiragana/static/media/た.2d7ad3e9.ogg"
+  },
+  {
+    "revision": "20da766ec906b60c2b448a9c74a923e8",
+    "url": "/learn-hiragana/static/media/ち.20da766e.ogg"
+  },
+  {
+    "revision": "4e356709768bba1e7317ca94691c5ac4",
+    "url": "/learn-hiragana/static/media/つ.4e356709.ogg"
+  },
+  {
+    "revision": "592d74d540fcc9878309e8c00680b255",
+    "url": "/learn-hiragana/static/media/て.592d74d5.ogg"
+  },
+  {
+    "revision": "431bd68751288384cfe4534493a6911e",
+    "url": "/learn-hiragana/static/media/と.431bd687.ogg"
+  },
+  {
+    "revision": "62679b0c3cf680126db7d636a18693e7",
+    "url": "/learn-hiragana/static/media/な.62679b0c.ogg"
+  },
+  {
+    "revision": "569d4c483fa6274df526ece962e52f87",
+    "url": "/learn-hiragana/static/media/に.569d4c48.ogg"
+  },
+  {
+    "revision": "853cbccbfedbc187df05c4d8b45e913b",
+    "url": "/learn-hiragana/static/media/ぬ.853cbccb.ogg"
+  },
+  {
+    "revision": "78302c1e593e0a0a18d5df4b0be4be19",
+    "url": "/learn-hiragana/static/media/ね.78302c1e.ogg"
+  },
+  {
+    "revision": "0ad299852798b29f2691acad9fc511ad",
+    "url": "/learn-hiragana/static/media/の.0ad29985.ogg"
+  },
+  {
+    "revision": "e21631a7eae154221d19070c64b1afb7",
+    "url": "/learn-hiragana/static/media/は.e21631a7.ogg"
+  },
+  {
+    "revision": "773b6cdf5c66b29a8d2ced84544b7110",
+    "url": "/learn-hiragana/static/media/ひ.773b6cdf.ogg"
+  },
+  {
+    "revision": "44380a2dc8dfe582e4ea0e37f3096fc1",
+    "url": "/learn-hiragana/static/media/ふ.44380a2d.ogg"
+  },
+  {
+    "revision": "262d6c8e6b0d555159f105d6c9d6cdec",
+    "url": "/learn-hiragana/static/media/へ.262d6c8e.ogg"
+  },
+  {
+    "revision": "c0ecac79207a61d9713f81ab495d9da4",
+    "url": "/learn-hiragana/static/media/ほ.c0ecac79.ogg"
+  },
+  {
+    "revision": "3459e17d4f63c9f26d7bc11f57a8e103",
+    "url": "/learn-hiragana/static/media/ま.3459e17d.ogg"
+  },
+  {
+    "revision": "3a566acd36b7bca4ee36ecacc528e7ec",
+    "url": "/learn-hiragana/static/media/み.3a566acd.ogg"
+  },
+  {
+    "revision": "eed3e4c582ce4f6aaf11e9d8edf704c5",
+    "url": "/learn-hiragana/static/media/む.eed3e4c5.ogg"
+  },
+  {
+    "revision": "c313e57b9b1881b9e70c83b00ca90a21",
+    "url": "/learn-hiragana/static/media/め.c313e57b.ogg"
+  },
+  {
+    "revision": "5876cd43f46129fc3f96b5fee77456cf",
+    "url": "/learn-hiragana/static/media/も.5876cd43.ogg"
+  },
+  {
+    "revision": "365c4c868bee76e55ecac277f9b2d612",
+    "url": "/learn-hiragana/static/media/や.365c4c86.ogg"
+  },
+  {
+    "revision": "8a518fb7a0a0c46bee971b542c596a60",
+    "url": "/learn-hiragana/static/media/ゆ.8a518fb7.ogg"
+  },
+  {
+    "revision": "200257b7ea437111943ce7219535c843",
+    "url": "/learn-hiragana/static/media/よ.200257b7.ogg"
+  },
+  {
+    "revision": "1751603101a55017c582a053699745f6",
+    "url": "/learn-hiragana/static/media/ら.17516031.ogg"
+  },
+  {
+    "revision": "19f8fa4ee398cedf2b0d0a175e2c332d",
+    "url": "/learn-hiragana/static/media/り.19f8fa4e.ogg"
+  },
+  {
+    "revision": "13b49104cdfe7f984068b546e89b91cb",
+    "url": "/learn-hiragana/static/media/る.13b49104.ogg"
+  },
+  {
+    "revision": "d364242d6693a0c12c2896121e4785ae",
+    "url": "/learn-hiragana/static/media/れ.d364242d.ogg"
+  },
+  {
+    "revision": "73579750cbcd5e5ed825cb0b5267b65b",
+    "url": "/learn-hiragana/static/media/ろ.73579750.ogg"
+  },
+  {
+    "revision": "c9cfa7a8a66facf5dc8d9d7994e6f51c",
+    "url": "/learn-hiragana/static/media/わ.c9cfa7a8.ogg"
+  },
+  {
+    "revision": "1473121fcd409451d15ba4eddc371675",
+    "url": "/learn-hiragana/static/media/を.1473121f.ogg"
+  },
+  {
+    "revision": "da8744ad1a34f5a877904b08a92b04f0",
+    "url": "/learn-hiragana/static/media/ん.da8744ad.ogg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d834066b3e683e7ded72f3c212d8df2.js"
+  "/learn-hiragana/precache-manifest.15f70b7e38aa382e1306a8db9913293d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/learn-hiragana/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
